942a8d1e0f04b2389fab17f9753b574c83bc41ba
SKambapu <skambapu@spireon.com>
2022-03-02 21:54:36 +0530

    Print All sub arrays

---
+++ src/com/personal/tryouts/PrintSubArrays.java
@@ -0,0 +1,27 @@
+package com.personal.tryouts;
+
+public class PrintSubArrays {
+
+    public static void main(String[] args) {
+        int a[] = {1,2,3};
+
+        printSubArrays(a, 0, 0);
+    }
+
+    static void printSubArrays(int[] a, int start, int end) {
+
+        if(a.length == end) {
+            return;
+        } else if (start > end) {
+            printSubArrays(a, 0, end+1);
+        } else {
+            System.out.print("[");
+            for(int i=start; i<end; i++) {
+                System.out.print(a[i]+ ",");
+            }
+            System.out.println(a[end] + "]");
+            printSubArrays(a, start+1, end);
+        }
+        return;
+    }
+}
