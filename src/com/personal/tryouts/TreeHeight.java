ae5dd56850997015ea26d08fb1edd6782fe6effc
SKambapu <skambapu@spireon.com>
2022-03-02 20:13:29 +0530

    Find Tree Diameter

--- src/com/personal/tryouts/TreeHeight.java
+++ src/com/personal/tryouts/TreeHeight.java
@@ -28,9 +28,60 @@ public class TreeHeight {
         root2.left.right = new TreeHeight.Node(5);
         root2.left.left.right = new TreeHeight.Node(6);
 
-        System.out.println(treeHeight.height(root1));
-        System.out.println(treeHeight.height(root2));
+        A a1 = new A();
+        System.out.println("Tree height = " + treeHeight.height(root1, a1) + " and diameter = " + treeHeight.diameter(root1));
+        A a2 = new A();
+        System.out.println("Tree height = " + treeHeight.height(root2, a2) + " and diameter = " + treeHeight.diameter(root2));
 
+
+        Node root = newNode(1);
+        root.left = newNode(2);
+        root.right = newNode(3);
+        root.left.left = newNode(4);
+        root.left.right = newNode(5);
+
+        System.out.println("Diameter is " + diameter(root));
+    }
+
+    static Node newNode(int data)
+    {
+        Node node = new Node(data);
+        return (node);
+    }
+
+    static class A
+    {
+        int ans = Integer.MIN_VALUE;
+    }
+
+    /* Function to find height of a tree */
+    static int height(Node root, A a)
+    {
+        if (root == null)
+            return 0;
+
+        int left_height = height(root.left, a);
+
+        int right_height = height(root.right, a);
+
+        // update the answer, because diameter of a
+        // tree is nothing but maximum value of
+        // (left_height + right_height + 1) for each node
+        a.ans = Math.max(a.ans, 1 + left_height +
+                right_height);
+
+        return 1 + Math.max(left_height, right_height);
+    }
+
+    static int diameter(Node root)
+    {
+        if (root == null)
+            return 0;
+
+        // This will store the final answer
+        A a = new A();
+        int height_of_tree = height(root, a);
+        return a.ans;
     }
 
     private int height(Node root) {
